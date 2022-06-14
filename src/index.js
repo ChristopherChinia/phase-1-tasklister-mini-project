3ef8dde6eb0d965bd2dd8a6921925e7a7cee3061
christopherchinia <chinia214@gmail.com>
2022-06-14 15:39:37 -0400

    done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let inPut = document.getElementById("new-task-description").value;
+    // create the todo list
+    let list = document.createElement("p");
+    // create the delete button
+    let button = document.createElement("button");
+    button.addEventListener("click", (e) => {
+      e.target.parentNode.remove();
+    });
+    // display on the button
+    button.textContent = "Done";
+    // content list
+    list.textContent = `${inPut} `;
+    // append button to list
+    list.appendChild(button);
+    // append list to ul idtask
+    document.querySelector("#tasks").appendChild(list);
+    document.querySelector("h1").remove();
+  });
 });
